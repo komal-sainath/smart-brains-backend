79d77fc846bbf2d1a582e26a0d16adda926dee8c
komal-sainath <komals1441997@gmail.com>
2019-10-04 12:21:37 +0530

    updating db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-	  host : '127.0.0.1',
+	  host : 'postgresql-aerodynamic-58844',
 	  user : 'komal',
 	  password : 'lillypotter07',
 	  database : 'smart-brain'
